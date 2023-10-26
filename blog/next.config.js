93e012861a9018120adf78af5cd5693197b2b291
Adam Griswold <adam.griswold@nucor.com>
2023-10-26 20:06:03 +0000

    ✨: enable code syntax highlighting

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,6 +1,12 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {
+    reactStrictMode: true, 
+    swcMinify: true, 
+    experimental: {
+        // https://stackoverflow.com/questions/75571651/using-remark-and-rehype-plugins-with-nextjs-13/75571708#75571708
+    }
+}
 
 module.exports = withContentlayer(nextConfig)
